329c49bd88a8015378b05e1d69848c8c76211e4c
Dale Wijnand <dale.wijnand@gmail.com>
2014-07-24 12:00:41 +0100

    Promising: Inline value class & inline promise loaner.

--- src/main/scala/beamly/core/lang/future/Promising.scala
+++ src/main/scala/beamly/core/lang/future/Promising.scala
@@ -2,7 +2,7 @@ package beamly.core.lang.future
 
 import scala.concurrent.{Future, Promise}
 
-class Promising[A](val promise: Promise[A]) extends AnyVal {
+@inline class Promising[A](val promise: Promise[A]) extends AnyVal {
   @inline def apply[B](f: Promise[A] => B): Future[A] = {
     f(promise)
     promise.future
