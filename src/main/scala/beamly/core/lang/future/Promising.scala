2e93aaa3fef161a908043d57a167138471152185
Dale Wijnand <dale.wijnand@gmail.com>
2014-07-24 11:51:22 +0100

    Promising: Make apply @inline.

--- src/main/scala/beamly/core/lang/future/Promising.scala
+++ src/main/scala/beamly/core/lang/future/Promising.scala
@@ -3,7 +3,7 @@ package beamly.core.lang.future
 import scala.concurrent.{Future, Promise}
 
 class Promising[A](val promise: Promise[A]) extends AnyVal {
-  def apply[B](f: Promise[A] => B): Future[A] = {
+  @inline def apply[B](f: Promise[A] => B): Future[A] = {
     f(promise)
     promise.future
   }
