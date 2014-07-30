4464f09e330c246c3844a06ee6d3786104abca9e
Dale Wijnand <dale.wijnand@gmail.com>
2014-07-30 18:08:27 +0100

    Add Failable, @derekjw's code.

---
+++ src/test/scala/beamly/core/lang/future/FailableTest.scala
@@ -0,0 +1,31 @@
+package beamly.core.lang.future
+
+import scala.language.higherKinds
+
+import beamly.core.lang.future.Failable._
+import org.specs2.mutable.Specification
+
+import scala.util.{Failure, Success}
+
+class FailableTest extends Specification {
+  val arithmeticException = new ArithmeticException("It's a zero!")
+
+  def foo[M[_, _]: Failable](n: Int): M[ArithmeticException, Int] =
+    if (n == 0)
+      Failable[M].failure(arithmeticException)
+    else
+      Failable[M].success(n)
+
+  "FutureEitherFailable" should {
+    "handle success" in foo[FutureEither](1).value ==== Some(Success(Right(1)))
+    "handle failure" in foo[FutureEither](0).value ==== Some(Success(Left(arithmeticException)))
+  }
+  "FutureIdFailable" should {
+    "handle success" in foo[FutureId](1).value ==== Some(Success(1))
+    "handle failure" in foo[FutureId](0).value ==== Some(Failure(arithmeticException))
+  }
+  "IdFailable" should {
+    "handle success" in foo[Id](1) ==== 1
+    "handle failure" in (foo[Id](0) must throwA(arithmeticException))
+  }
+}
